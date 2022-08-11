7e8923f1f2fda7220855e341e293591e66e5e220
Goran <dobrilovic.goran@gmail.com>
2022-08-11 09:23:07 +0200

    finalna verzija sql skripte

--- porudzbine-backend/src/database/baza-definicija.sql
+++ porudzbine-backend/src/database/baza-definicija.sql
@@ -1,4 +1,5 @@
--- Napravi u repository sloju neke DBKorisnikModel i to uvek pre vracanja kovertuj u KorisnikModel, tako uradi za sve ove modele
+SET TIME ZONE 'Europe/Belgrade';
+
 CREATE TABLE korisnik (
     korisnik_id serial PRIMARY KEY,
     korisnik_ime TEXT NOT NULL,
@@ -28,6 +29,24 @@ CREATE TABLE jelo (
         ON DELETE CASCADE
 );
 
+CREATE TABLE porudzbina (
+    porudzbina_id serial PRIMARY KEY,
+    porudzbina_opis TEXT NULL,
+    porudzbina_korisnik_id INT NOT NULL,
+    porudzbina_status TEXT NOT NULL default 'PRIMLJENO',
+    porudzbina_kreirana TIMESTAMP default (now())
+);
+
+CREATE TABLE porudzbina_jelo (
+    pj_porudzbina_id INT NOT NULL,
+    pj_jelo_id INT NOT NULL,
+    pj_jelo_cena DECIMAL NOT NULL,
+    pj_jelo_naziv TEXT NOT NULL,
+
+    CONSTRAINT pj_porudzbina_id_fk FOREIGN KEY (pj_porudzbina_id) REFERENCES porudzbina(porudzbina_id),
+    CONSTRAINT pj_jelo_id_fk FOREIGN KEY (pj_jelo_id) REFERENCES jelo(jelo_id)
+);
+
 -- Sifra za svakog korisnika je testSifra 
 INSERT INTO korisnik (korisnik_ime, korisnik_email, korisnik_sifra, korisnik_uloga)
     VALUES ('Djordje Djordjevic', 'dj@gmail.com', '$2a$12$c9kp0O9IiFoJA5hycufU7OrbToXjzXhKG24wVVyHl6gIwssGOHfX6', 'ADMIN'), 
@@ -43,3 +62,13 @@ INSERT INTO jelo (jelo_naziv, jelo_sastojci, jelo_cena, jelo_porcija, kategorija
     VALUES ('Jaja i slanine', 'Jaja, slanina', 300.00, '2 jaja, 1 slanina', 1),
            ('Jagnjetina ispod saca', 'Jagnjetina, krompir', 1250.00, '500gr', 2),
            ('Supa', 'Brokoli, rezanca, sargarepa', 125.50, NULL, 3);
+
+INSERT INTO porudzbina (porudzbina_opis, porudzbina_korisnik_id) 
+    VALUES (NULL, 2),
+           (NULL, 3),
+           ('Supa sa manje rezanaca', 2);
+
+INSERT INTO porudzbina_jelo (pj_porudzbina_id, pj_jelo_id, pj_jelo_cena, pj_jelo_naziv)
+    VALUES (1, 1, 300, 'Jaja i slanine'),
+           (2, 2, 1250.50, 'Jagnjetina ispod saca'),
+           (3, 2, 125, 'Supa');
